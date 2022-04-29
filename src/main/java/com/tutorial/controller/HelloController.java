c592c112f09f4737a709b8a61e57c9e63dd4a5d4
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2022-04-28 23:46:03 -0500

    xd

---
+++ src/main/java/com/tutorial/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.tutorial.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@Value("${app.message}")
+	String message;
+	
+	@GetMapping("/api/hola")
+	public String hola() {
+		System.out.println("xd"+message);
+		return "Hi";
+	}
+}
